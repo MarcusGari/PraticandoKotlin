8b5f674eeffe86b91436232352ed6d89239980dc
MarcusGari <marcus_gari@yahoo.com.br>
2021-03-09 00:05:53 -0300

    Operations test file updated

--- src/main/kotlin/me/user/TesteOperacoes.kt
+++ src/main/kotlin/me/user/TesteOperacoes.kt
@@ -15,4 +15,16 @@ fun main(){
     println(onda + "\nFiltrando com .filter{}:\n" + onda)
     val maiorque2500 = salarios.filter { it > 2500.0 }
     maiorque2500.forEach{ println(it)}
+
+    println(onda + "\nContando elementos entre 2000 e 5000 com .count{}:\n" + onda)
+    println(salarios.count{it in 2000.0..5000.0})
+
+    println(onda + "\nProcurando elementos iguais a 2250 e 500 com .find{}:\n" + onda)
+    println(salarios.find{it == 2250.0})
+    println(salarios.find{it == 500.0})
+
+    println(onda + "\nBuscando elementos iguais a 1000 e 500 com .any{}:\n" + onda)
+    println(salarios.any{it == 1000.0})
+    println(salarios.any{it == 500.0})
+
 }
